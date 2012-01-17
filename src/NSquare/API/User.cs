f816f033e5da431dc1aeac1a41d6d746c6381b27
unknown <brezende@brezende-win8vm.(none)>
2012-01-16 23:21:18 -0200

    ADD: New unit tests and User class

---
+++ src/NSquare/API/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Amarok.Framework.Contracts;
+
+namespace NSquare.API
+{
+    public class User
+    {
+        public string Id { get; private set; }
+
+        public User(string id)
+        {
+            Ensure.That(id).IsNotNull().Otherwise.Throw<ArgumentNullException>();
+            this.Id = id;
+        }
+    }
+}
---
+++ tests/NSquare.Tests/API/UserTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NSquare.API;
+using NUnit.Framework;
+
+namespace NSquare.Tests.API
+{
+    public class UserTests
+    {
+        [Test]
+        public void User_Has_Id_And_It_Must_Be_Readonly()
+        {
+            string userId = "156745487";
+            User user = new User(userId);
+
+            Assert.AreEqual(userId, user.Id);
+        }
+
+        [Test]
+        public void When_User_Id_Is_Not_Provided_It_Must_Throws_Exception()
+        {
+            TestDelegate createUserWithNullableId = new TestDelegate(() => { new User(null); });
+            TestDelegate createUserWithEmptyId = new TestDelegate(() => { new User(String.Empty); });
+
+            Assert.Throws<ArgumentNullException>(createUserWithNullableId);
+        }
+    }
+}

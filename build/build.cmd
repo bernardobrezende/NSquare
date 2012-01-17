a0ad369986b35f593fdb52236d5e801ba9cebecd
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-01-17 12:02:39 -0200

    ADD: build script

---
+++ build/build.cmd
@@ -0,0 +1,3 @@
+::If needed, run this command: set PATH=%PATH%;%WINDIR%\Microsoft.Net\Framework\v4.0.30319
+
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" build.xml %~1 %2 %3 %4 %5 %6 %7 %8 %9
